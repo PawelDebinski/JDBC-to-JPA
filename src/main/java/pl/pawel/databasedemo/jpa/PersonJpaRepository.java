a6854891f8b7467dea35c8b4d84845a69643fd11
Pawel Debinski <spokojasne@interia.eu>
2018-09-15 09:41:40 +0200

    Implemented findByID - Jpa, closes #10

---
+++ src/main/java/pl/pawel/databasedemo/jpa/PersonJpaRepository.java
@@ -0,0 +1,23 @@
+package pl.pawel.databasedemo.jpa;
+
+import org.springframework.stereotype.Repository;
+import pl.pawel.databasedemo.entity.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public class PersonJpaRepository {
+
+    // connect to the database
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Person findById(int id) {
+        return entityManager.find(Person.class, id);
+    }
+
+
+}
